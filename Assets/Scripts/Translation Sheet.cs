0537862d3ee8c1fcbf2d52677b9433c5ed229df0
JoePompey <up2274763@myport.ac.uk>
2026-01-19 13:34:51 +0000

    Island Name Sheet
    - Added island name sheet.
    - Press I to open sheet.
    - Translations sheet and island name sheet will close the other to open them.

--- Assets/Scripts/Translation Sheet.cs
+++ Assets/Scripts/Translation Sheet.cs
@@ -1,22 +1,38 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class TranslationSheet : MonoBehaviour
 {
-    private SpriteRenderer[] AllSprites;
-    private bool Visible = false;
+    //Initialising sprite lists and visibilities.
+    private SpriteRenderer[] AllTranslationSprites;
+    private SpriteRenderer[] AllIslandNameSprites;
+    private bool TranslationVisible = false;
+    private bool IslandNameVisible = false;
+    //.
 
+    //Initialising controls.
     public InputActionAsset controls;
     private InputActionMap ArmControls;
+    //.
 
     void Start()
     {
+        //Declaring controls.
         ArmControls = controls.FindActionMap("ArmMovements");
         ArmControls.Enable();
-        
-        AllSprites = GetComponentsInChildren<SpriteRenderer>();
-        SpriteVisibility(false);
+        //.
+
+        //Declaring sprites.
+        Transform Translations = transform.Find("Translations");
+        AllTranslationSprites = Translations.GetComponentsInChildren<SpriteRenderer>();
+
+        Transform IslandNames = transform.Find("IslandNames");
+        AllIslandNameSprites = IslandNames.GetComponentsInChildren<SpriteRenderer>();
+        //.
+
+        SpriteVisibility(false, false);
     }
 
     //Controls for toggling visibility.
@@ -24,18 +40,61 @@ public class TranslationSheet : MonoBehaviour
     {
         if (ArmControls.FindAction("OpenTranslations").WasPressedThisFrame())
         {
-            Visible = !Visible;
-            SpriteVisibility(Visible);
+            if (IslandNameVisible)
+            {
+                SpriteVisibility(true, false);
+                TranslationVisible = true;
+                IslandNameVisible = false;
+            }
+            else if (TranslationVisible)
+            {
+                SpriteVisibility(false, false);
+                TranslationVisible = false;
+                IslandNameVisible = false;
+            }
+            else 
+            {
+                SpriteVisibility(true, false);
+                TranslationVisible = true;
+                IslandNameVisible = false;
+            }
+        }
+
+        if (ArmControls.FindAction("OpenIslandNames").WasPressedThisFrame())
+        {
+            if (TranslationVisible)
+            {
+                SpriteVisibility(false, true);
+                TranslationVisible = false;
+                IslandNameVisible = true;
+            }
+            else if (IslandNameVisible)
+            {
+                SpriteVisibility(false, false);
+                TranslationVisible = false;
+                IslandNameVisible = false;
+            }
+            else
+            {
+                SpriteVisibility(false, true);
+                TranslationVisible = false;
+                IslandNameVisible = true;
+            }
         }
     }
     //.
 
     //Enable/disable sheet visibility.
-    void SpriteVisibility(bool Visibility)
+    void SpriteVisibility(bool TranslationVisibility, bool IslandNameVisibility)
     {
-        foreach (var SingleSprite in AllSprites)
+        foreach (var SingleSprite in AllTranslationSprites)
         {
-            SingleSprite.enabled = Visibility;
+            SingleSprite.enabled = TranslationVisibility;
+        }
+
+        foreach (var SingleSprite in AllIslandNameSprites)
+        {
+            SingleSprite.enabled = IslandNameVisibility;
         }
     }
     //.
